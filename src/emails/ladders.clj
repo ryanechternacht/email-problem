00634f743ed20833ca3dfede3e51868b466c8976
ryanechternacht <ryan@echternacht.org>
2020-04-26 00:15:54 -0400

    Change main to use channels

--- src/emails/ladders.clj
+++ src/emails/ladders.clj
@@ -1,14 +1,25 @@
 (ns emails.ladders
   (:require [emails.load-emails :as le]
-            [emails.process-emails :as pe])
+            [emails.process-emails :as pe]
+            [clojure.core.async :refer [go chan <! >! <!! >!! alts!]])
   (:gen-class))
 
 (def defaults {:file "resources/sample-emails-100"})
 
+(def xf (pe/generate-xf))
+
+(def potential-emails (chan 10 xf))
+
+(def to-email (atom []))
+
+(go (while true
+      (let [email (<! potential-emails)]
+        (swap! to-email conj email))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [xf (pe/generate-xf {})]
-    (->> (:file defaults)
-         le/load-emails-from-file
-         (into [] xf))))
+  (let [emails (le/load-emails-from-file (:file defaults))]
+    (for [e emails]
+      (go
+        (>! potential-emails e)))))
