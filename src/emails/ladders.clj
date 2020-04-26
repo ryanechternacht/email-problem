574214a78f598269d96c2312ef9725a6da66669f
ryanechternacht <ryan@echternacht.org>
2020-04-26 11:45:55 -0400

    Make the simple version simple

--- src/emails/ladders.clj
+++ src/emails/ladders.clj
@@ -6,20 +6,31 @@
 
 (def defaults {:file "resources/sample-emails-100"})
 
-(def xf (pe/generate-xf))
+;; (def xf (pe/generate-xf))
 
-(def potential-emails (chan 10 xf))
+;; (def potential-emails (chan 10 xf))
 
-(def to-email (atom []))
+;; (def to-email (atom []))
 
-(go (while true
-      (let [email (<! potential-emails)]
-        (swap! to-email conj email))))
+;; (go (while true
+;;       (let [email (<! potential-emails)]
+;;         (swap! to-email conj email))))
+
+(defn- run-simple []
+  (let [file (:file defaults)
+        xf (pe/generate-xf)]
+    (->> file
+         le/load-emails-from-file
+         (into [] xf))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (let [emails (le/load-emails-from-file (:file defaults))]
-    (for [e emails]
-      (go
-        (>! potential-emails e)))))
+  (run-simple))
+
+;; (defn -main
+;;   "I don't do a whole lot ... yet."
+;;   [& args]
+;;   (let [emails (le/load-emails-from-file (:file defaults))]
+;;     (for [e emails]
+;;       (go
+;;         (>! potential-emails e)))))
